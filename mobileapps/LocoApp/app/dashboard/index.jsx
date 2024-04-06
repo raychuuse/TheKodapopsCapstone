9575213cb5fee24e18e21d859675295cc892ef2a
Andrew Wilks <a.wilksy92@gmail.com>
2024-04-06 20:12:00 +1000

    Added Siding Cards and added the siding list to the main dashbord with mock data. LocoApp

--- mobileapps/LocoApp/app/dashboard/index.jsx
+++ mobileapps/LocoApp/app/dashboard/index.jsx
@@ -1,4 +1,5 @@
-import { View } from "react-native";
+import { View, Animated, Text } from "react-native";
+import React, { useState } from "react";
 
 // Components
 import StatusIndicator from "../../components/statusIndicator";
@@ -6,7 +7,69 @@ import UserGreeting from "../../components/userGreeting";
 import SelectedSiddingStats from "../../components/selectedSidingStats";
 import SidingCard from "../../components/sidingCard";
 
+const sidingData = [
+  {
+    id: 1,
+    isCompleted: true,
+    isSelected: false,
+    name: "Siding #1",
+    drop: 5,
+    collect: 10,
+  },
+  {
+    id: 2,
+    isCompleted: false,
+    isSelected: false,
+    name: "Siding #2",
+    drop: 5,
+    collect: 10,
+  },
+  {
+    id: 3,
+    isCompleted: false,
+    isSelected: true,
+    name: "Siding #3",
+    drop: 5,
+    collect: 10,
+  },
+  {
+    id: 4,
+    isCompleted: false,
+    isSelected: false,
+    name: "Siding #4",
+    drop: 5,
+    collect: 10,
+  },
+  {
+    id: 5,
+    isCompleted: false,
+    isSelected: false,
+    name: "Siding #5",
+    drop: 5,
+    collect: 10,
+  },
+  {
+    id: 6,
+    isCompleted: false,
+    isSelected: false,
+    name: "Siding #6",
+    drop: 5,
+    collect: 10,
+  },
+  {
+    id: 7,
+    isCompleted: false,
+    isSelected: false,
+    name: "Siding #7",
+    drop: 5,
+    collect: 10,
+  },
+];
+
 export default function Page() {
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+  const [sidingCarouselWidth, setSidingCarouselWidth] = useState(0);
+
   return (
     <View style={{ flex: 1 }}>
       <View
@@ -22,15 +85,37 @@ export default function Page() {
       </View>
       <View style={{ flex: 1, paddingHorizontal: 128, paddingVertical: 32, gap: 32 }}>
         <View
+          onLayout={(event) => setSidingCarouselWidth(event.nativeEvent.layout.width)}
           style={{
             flex: 1,
             backgroundColor: "rgba(255,255,255,0.6)",
             borderRadius: 16,
             flexDirection: "row",
           }}>
-          <SidingCard isCompleted />
-          <SidingCard isSelected />
-          <SidingCard />
+          <Animated.FlatList
+            contentContainerStyle={{ alignItems: "center", paddingVertical: 10, gap: 16 }}
+            snapToInterval={85}
+            decelerationRate={0}
+            scrollEventThrottle={16}
+            onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: scrollX } } }], { useNativeDriver: true })}
+            bounces={false}
+            horizontal={true}
+            keyExtractor={(item) => item.id}
+            data={sidingData}
+            renderItem={({ item, index }) => (
+              <SidingCard
+                isCompleted={item.isCompleted}
+                isSelected={item.isSelected}
+                name={item.name}
+                drop={item.drop}
+                collect={item.collect}
+                index={index}
+                scrollX={scrollX}
+                containerWidth={sidingCarouselWidth}
+                listLength={sidingData.length}
+              />
+            )}
+          />
         </View>
         <View
           style={{
--- mobileapps/LocoApp/components/sidingCard.jsx
+++ mobileapps/LocoApp/components/sidingCard.jsx
@@ -1,105 +1,112 @@
-import { useState } from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { useState } from "react";
+import { View, StyleSheet, TouchableOpacity, Animated } from "react-native";
+import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
 
 // Styles
-import { Title3, Headline, H1 } from '../styles/typography';
-import { Colours } from '../styles/colours';
+import { Title3, Headline, H1 } from "../styles/typography";
+import { Colours } from "../styles/colours";
 
-// Components
-import Button from './button';
-
-export default function SidingCard({ isCompleted = false, isSelected = false, name = 'Siding Name', drop = 888, collect = 888 }) {
+const SidingCard = ({ containerWidth, index, listLength, isCompleted = false, isSelected = false, name: name = "Siding Name", drop = 888, collect = 888, selectedHandeler, scrollX }) => {
   const [_isCompleted, setIsCompleted] = useState(isCompleted);
   const [_isSelected, setIsSelected] = useState(isSelected);
+  const inputRange = [(index - 1) * 170, index * 170, (index + 1) * 170];
+  const translateY = scrollX.interpolate({
+    inputRange,
+    outputRange: [0, -10, 0],
+  });
 
-  function selectedHandeler() {
-    setIsSelected(!_isSelected);
+  // TODO: REMOVE when a selectedHandeler is implemented!!!
+  if (selectedHandeler == null) {
+    selectedHandeler = () => {
+      setIsSelected(!_isSelected);
+    };
   }
+
   return (
-    <View style={[styles.card, styles.default, _isCompleted ? styles.complete : null, _isSelected ? styles.selected : null]}>
-      <View style={{ flex: 2, justifyContent: 'center' }}>
-        <Title3 style={[styles.defaultText, _isCompleted ? styles.completeText : null, _isSelected ? styles.selectedText : null]}>{name}</Title3>
+    <Animated.View
+      style={[
+        styles.card,
+        styles.default,
+        _isCompleted ? styles.complete : _isSelected ? styles.selected : null,
+        { transform: [{ translateY }] },
+        index == 0 ? { marginLeft: (containerWidth - 170) / 2 } : null,
+        index == listLength - 1 ? { marginRight: (containerWidth - 170) / 2 } : null,
+      ]}>
+      <View style={{ flex: 3, justifyContent: "center" }}>
+        <Title3 style={[styles.defaultText, _isCompleted ? styles.completeText : _isSelected ? styles.selectedText : null]}>{name}</Title3>
       </View>
       <View style={styles.devider} />
       <View style={styles.binCounterContainer}>
         <View style={styles.binCounterLabel}>
-          <MaterialIcons
-            name='download'
-            size={18}
-            color={(Colours.spPendingText, _isCompleted ? Colours.spCompleteText : null, _isSelected ? Colours.spSelectedText : null)}
-          />
-          <Headline style={[{ marginRight: 9 }, styles.defaultText, _isCompleted ? styles.completeText : null, _isSelected ? styles.selectedText : null]}>Drop Off</Headline>
+          <MaterialIcons name="download" size={18} color={(Colours.spPendingText, _isCompleted ? Colours.spCompleteText : _isSelected ? Colours.spSelectedText : null)} />
+          <Headline style={[{ marginRight: 9 }, styles.defaultText, _isCompleted ? styles.completeText : _isSelected ? styles.selectedText : null]}>Drop Off</Headline>
         </View>
-        <H1 style={[styles.binCounter, _isCompleted ? styles.completeText : null, _isSelected ? styles.selectedText : null]}>{drop}</H1>
+        <H1 style={[styles.binCounter, _isCompleted ? styles.completeText : _isSelected ? styles.selectedText : null]}>{drop}</H1>
       </View>
       <View style={styles.devider} />
       <View style={styles.binCounterContainer}>
         <View style={styles.binCounterLabel}>
-          <MaterialIcons
-            name='upload'
-            size={18}
-            color={(Colours.spPendingText, _isCompleted ? Colours.spCompleteText : null, _isSelected ? Colours.spSelectedText : null)}
-          />
-          <Headline style={[{ marginRight: 9 }, styles.defaultText, _isCompleted ? styles.completeText : null, _isSelected ? styles.selectedText : null]}>Collect</Headline>
+          <MaterialIcons name="upload" size={18} color={(Colours.spPendingText, _isCompleted ? Colours.spCompleteText : _isSelected ? Colours.spSelectedText : null)} />
+          <Headline style={[{ marginRight: 9 }, styles.defaultText, _isCompleted ? styles.completeText : _isSelected ? styles.selectedText : null]}>Collect</Headline>
         </View>
-        <H1 style={[styles.binCounter, _isCompleted ? styles.completeText : null, _isSelected ? styles.selectedText : null]}>{collect}</H1>
+        <H1 style={[styles.binCounter, _isCompleted ? styles.completeText : _isSelected ? styles.selectedText : null]}>{collect}</H1>
       </View>
       <View style={styles.devider} />
-      <View style={{ flex: 2 }}>
-        <TouchableOpacity onPress={selectedHandeler}>
+      <View style={{ flex: 3, justifyContent: "center" }}>
+        <TouchableOpacity onPress={selectedHandeler} disabled={isCompleted}>
           <MaterialCommunityIcons
-            name={_isCompleted ? 'checkbox-marked-circle-outline' : _isSelected ? 'star-circle-outline' : 'checkbox-blank-circle-outline'}
-            size={48}
-            color={(Colours.spPendingText, _isCompleted ? Colours.spCompleteText : null, _isSelected ? Colours.spSelectedText : null)}
+            name={_isCompleted ? "checkbox-marked-circle-outline" : _isSelected ? "star-circle-outline" : "checkbox-blank-circle-outline"}
+            size={36}
+            color={(Colours.spPendingText, _isCompleted ? Colours.spCompleteText : _isSelected ? Colours.spSelectedText : null)}
           />
         </TouchableOpacity>
       </View>
-    </View>
+    </Animated.View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   binCounterContainer: {
-    flex: 4,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
+    flex: 6,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
   },
   binCounterLabel: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 4,
   },
   binCounter: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 51,
-    fontWeight: '800',
+    fontWeight: "800",
     lineHeight: 61,
     color: Colours.spPendingText,
     marginVertical: 0,
   },
   devider: {
     height: 1,
-    width: '70%',
+    width: "70%",
     backgroundColor: Colours.spPendingText,
   },
   debug: {
-    borderStyle: 'dashed',
-    borderColor: 'red',
+    borderStyle: "dashed",
+    borderColor: "red",
     borderWidth: 1,
   },
   card: {
-    borderStyle: 'solid',
-    borderColor: '#fff',
+    borderStyle: "solid",
+    borderColor: "#fff",
     borderTopWidth: 4,
     borderLeftWidth: 2,
     borderRightWidth: 2,
     borderRadius: 15,
-    margin: 10,
+    // margin: 10,
     padding: 10,
-    width: 200,
-    alignItems: 'center',
+    width: 170,
+    alignItems: "center",
+    height: "90%",
   },
   default: {
     backgroundColor: Colours.spPending,
@@ -123,3 +130,5 @@ const styles = StyleSheet.create({
     color: Colours.spSelectedText,
   },
 });
+
+export default SidingCard;
