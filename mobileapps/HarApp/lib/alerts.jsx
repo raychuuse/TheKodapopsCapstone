698faa3befd9fa36553ee57b9c652561c0d936f4
Andrew Wilks <a.wilksy92@gmail.com>
2024-04-08 14:42:14 +1000

    Switched around the yest options to be consistantly on the right

--- mobileapps/HarApp/lib/alerts.jsx
+++ mobileapps/HarApp/lib/alerts.jsx
@@ -1,16 +1,17 @@
-import { Alert } from "react-native";
-import { router } from "expo-router";
+import { Alert } from 'react-native';
+import { router } from 'expo-router';
 
 export const FinishedAlert = () => {
-  Alert.alert("Finished?", "\nAre you sure you're finished at this Siding?", [
+  Alert.alert('Finished?', "\nAre you sure you're finished at this Siding?", [
     { text: "I'm not" },
-    { text: "Yes, I am!", onPress: router.back },
+    { text: 'Yes, I am!', onPress: router.back },
   ]);
 };
 
 export const RemoveBinAlert = (message) => {
-  Alert.alert("Confirm Missing Bin?", `\nAre you sure you want to report ${message} as missing?`, [
-    { text: "Yes, Report" },
-    { text: "No, It's Here!" },
-  ]);
+  Alert.alert(
+    'Confirm Missing Bin?',
+    `\nAre you sure you want to report ${message} as missing?`,
+    [{ text: "No, It's Here!" }, { text: 'Yes, Report' }]
+  );
 };
