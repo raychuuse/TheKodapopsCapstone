b9c56c9317ca1ca0f8286e50f2633b8f4b4d04e0
Andrew Wilks <a.wilksy92@gmail.com>
2024-04-11 09:49:36 +1000

    Added the import for the runsheet component and fixed the sizing of the runsheet modal. LocoApp

--- mobileapps/LocoApp/components/bottomBar.jsx
+++ mobileapps/LocoApp/components/bottomBar.jsx
@@ -1,15 +1,15 @@
-import { View } from "react-native";
-import { useState } from "react";
-import { BlurView } from "expo-blur";
-import { LinearGradient } from "expo-linear-gradient";
+import { View } from 'react-native';
+import { useState } from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
 
 // Styles
-import { Colours } from "../styles/colours";
+import { Colours } from '../styles/colours';
 
 // Components
-import NotificationBell from "./notificationBell";
-import Button from "./button";
-import CustomModal from "./modal";
+import NotificationBell from './notificationBell';
+import Button from './button';
+import CustomModal from './modal';
+import RunSheet from './runSheet';
 
 const BottomBar = () => {
   const [settingsVisible, setSettingsVisible] = useState(false);
@@ -22,49 +22,65 @@ const BottomBar = () => {
       <CustomModal
         isVisible={settingsVisible}
         onClose={() => setSettingsVisible(false)}
-        style={{ width: "80%", maxWidth: 800, height: "70%" }}></CustomModal>
+        style={{ width: '80%', maxWidth: 800, height: '70%' }}
+      ></CustomModal>
       {/* Notifications Modal */}
       <CustomModal
         isVisible={notificationsVisible}
         onClose={() => setNotificationsVisible(false)}
-        style={{ width: "80%", maxWidth: 800, height: "70%" }}></CustomModal>
+        style={{ width: '80%', maxWidth: 800, height: '70%' }}
+      ></CustomModal>
       {/* Route Modal */}
       <CustomModal
         isVisible={routeVisible}
         onClose={() => setRouteVisible(false)}
-        style={{ width: "80%", maxWidth: 800, height: "70%" }}>
+        style={{ width: '85%', height: '90%' }}
+        buttonIcon=''
+      >
         <RunSheet onClose={() => setRouteVisible(false)} />
       </CustomModal>
       {/* Help Modal */}
       <CustomModal
         isVisible={helpVisible}
         onClose={() => setHelpVisible(false)}
-        style={{ width: "80%", maxWidth: 800, height: "70%" }}></CustomModal>
+        style={{ width: '80%', maxWidth: 800, height: '70%' }}
+      ></CustomModal>
       {/* Nav Bar */}
       <LinearGradient
-        colors={["rgba(225,225,225,0.7)", "rgba(225,225,225,0.4)"]}
+        colors={['rgba(225,225,225,0.7)', 'rgba(225,225,225,0.4)']}
         style={{
-          flexDirection: "row",
+          flexDirection: 'row',
           paddingLeft: 32,
           paddingRight: 16,
           paddingTop: 4,
-        }}>
+        }}
+      >
         <NotificationBell
-          backgroundColor="transparent"
+          backgroundColor='transparent'
           iconColor={Colours.textLevel3}
           notificationCount={5}
           iconSize={48}
           onPress={() => setNotificationsVisible(true)}
         />
-        <View style={{ marginStart: "auto", flexDirection: "row", gap: 8 }}>
-          <Button backgroundColor="transparent" iconName="route" iconSize={48} onPress={() => setRouteVisible(true)} />
+        <View style={{ marginStart: 'auto', flexDirection: 'row', gap: 8 }}>
           <Button
-            backgroundColor="transparent"
-            iconName="settings"
+            backgroundColor='transparent'
+            iconName='route'
+            iconSize={48}
+            onPress={() => setRouteVisible(true)}
+          />
+          <Button
+            backgroundColor='transparent'
+            iconName='settings'
             iconSize={48}
             onPress={() => setSettingsVisible(true)}
           />
-          <Button backgroundColor="transparent" iconName="help" iconSize={48} onPress={() => setHelpVisible(true)} />
+          <Button
+            backgroundColor='transparent'
+            iconName='help'
+            iconSize={48}
+            onPress={() => setHelpVisible(true)}
+          />
         </View>
       </LinearGradient>
     </>
