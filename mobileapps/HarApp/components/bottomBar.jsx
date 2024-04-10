b5e93d3797513bd0b9e1f2b338747e7dbfcd19d6
Andrew Wilks <a.wilksy92@gmail.com>
2024-04-10 16:22:20 +1000

    Refactored bottom bar into its own component. HarApp

---
+++ mobileapps/HarApp/components/bottomBar.jsx
@@ -0,0 +1,51 @@
+import { View, StyleSheet } from 'react-native';
+
+// Import Components
+import Button from './button';
+import NotificationBell from './notificationBell';
+
+const BottomBar = ({
+  notifications,
+  setNotificationVisable,
+  setTutorialVisable,
+  setSettingsVisable,
+}) => {
+  return (
+    <View
+      style={{
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 16,
+        paddingLeft: 16,
+        paddingRight: 32,
+      }}
+    >
+      <View style={{ flexDirection: 'row' }}>
+        <Button
+          iconName='settings'
+          backgroundColor='transparent'
+          iconSize={48}
+          iconColor='#fff'
+          onPress={() => setSettingsVisable(true)}
+        />
+
+        <Button
+          iconName='help-outline'
+          backgroundColor='transparent'
+          iconSize={48}
+          iconColor='#fff'
+          onPress={() => setTutorialVisable(true)}
+        />
+      </View>
+      <NotificationBell
+        backgroundColor='transparent'
+        iconSize={48}
+        notificationCount={notifications.length}
+        onPress={() => setNotificationVisable(true)}
+      />
+    </View>
+  );
+};
+
+export default BottomBar;
