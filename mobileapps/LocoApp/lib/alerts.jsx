497b5ff94ae30a5414d200169eeaef090f97bf84
Andrew Wilks <a.wilksy92@gmail.com>
2024-04-14 18:48:01 +1000

    Added the alerts lib used in the project. LocoApp

---
+++ mobileapps/LocoApp/lib/alerts.jsx
@@ -0,0 +1,25 @@
+import { Alert } from 'react-native';
+import { router } from 'expo-router';
+
+export const FinishedAlert = () => {
+  Alert.alert('Finished?', "\nAre you sure you're finished at this Siding?", [
+    { text: "I'm not" },
+    { text: 'Yes, I am!', onPress: router.back },
+  ]);
+};
+
+export const RemoveBinAlert = (message) => {
+  Alert.alert(
+    'Confirm Missing Bin?',
+    `\nAre you sure you want to report ${message} as missing?`,
+    [{ text: "No, It's Here!" }, { text: 'Yes, Report' }]
+  );
+};
+
+export const RepairBinAlert = (message) => {
+  Alert.alert(
+    'Confirm Bin Repair?',
+    `\nAre you sure you want to request a repair for Bin #${message}?`,
+    [{ text: "No, It's Fine!" }, { text: 'Yes, Request' }]
+  );
+};
