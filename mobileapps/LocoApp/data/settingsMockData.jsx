d6889aefa29453a587b5d57445c5601e66aa1bca
Andrew Wilks <a.wilksy92@gmail.com>
2024-04-14 22:29:33 +1000

    added settings mock data to the project. LocoApp

---
+++ mobileapps/LocoApp/data/settingsMockData.jsx
@@ -0,0 +1,9 @@
+export const SettingsMockData_Loco = [
+  { label: 'Loco #11', id: 1 },
+  { label: 'Loco #22', id: 2 },
+  { label: 'Loco #33', id: 3 },
+];
+export const SettingMockData_Run = [
+  { label: 'Run 11', id: 1 },
+  { label: 'Run 22', id: 2 },
+];
--- mobileapps/LocoApp/pages/setup.jsx
+++ mobileapps/LocoApp/pages/setup.jsx
@@ -1,15 +1,21 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
 
 // Import Components
-import Button from "../components/button";
-import GreetingMessage from "../lib/greetingMessage";
-import SettingsItem from "../components/settingsItem";
+import Button from '../components/button';
+import GreetingMessage from '../lib/greetingMessage';
+import SettingsItem from '../components/settingsItem';
 
 // Import Styling Components
-import { LargeTitle, Title1 } from "../styles/typography";
-import { Colours } from "../styles/colours";
+import { LargeTitle, Title1 } from '../styles/typography';
+import { Colours } from '../styles/colours';
+
+// Import Mock Data
+import {
+  SettingsMockData_Loco,
+  SettingMockData_Run,
+} from '../data/settingsMockData';
 
 const SetupPage = () => {
   return (
@@ -24,15 +30,41 @@ const SetupPage = () => {
         </View>
         {/* Page Content */}
         <View style={styles.content}>
-          <SettingsItem type="train" label="Locomotive" body="Loco #123456" />
+          {/* Locomotive Selector */}
+          <SettingsItem
+            label='Locomotive'
+            options={SettingsMockData_Loco}
+            startOption={SettingsMockData_Loco[0].id}
+          />
+          {/* Run Selector */}
+          <SettingsItem
+            label='Run'
+            options={SettingMockData_Run}
+            startOption={SettingMockData_Run[0].id}
+          />
         </View>
         {/* Actions */}
         <View style={styles.actions}>
-          <Link href="/" asChild>
-            <Button title="Log Out" textColor={Colours.textLevel2} backgroundColor="transparent" border />
+          <Link
+            href='/'
+            asChild
+          >
+            <Button
+              title='Log Out'
+              textColor={Colours.textLevel2}
+              backgroundColor='transparent'
+              border
+            />
           </Link>
-          <Link href="/dashboard" asChild>
-            <Button title="Start" textColor={Colours.textLevel3} style={StyleSheet.create({ flex: 1 })} />
+          <Link
+            href='/dashboard'
+            asChild
+          >
+            <Button
+              title='Start'
+              textColor={Colours.textLevel3}
+              style={StyleSheet.create({ flex: 1 })}
+            />
           </Link>
         </View>
       </View>
@@ -43,45 +75,45 @@ const SetupPage = () => {
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#574294",
-    padding: "6%",
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#574294',
+    padding: '6%',
   },
   body: {
     flex: 1,
-    backgroundColor: "rgba(255, 255, 255, 0.8)",
-    alignItems: "center",
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    alignItems: 'center',
     borderRadius: 32,
     padding: 16,
     paddingTop: 32,
     marginTop: 8,
     marginHorizontal: 16,
     gap: 16,
-    width: "80%",
+    width: '80%',
     maxWidth: 500,
   },
   nav: {
-    width: "100%",
+    width: '100%',
 
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   actions: {
-    width: "100%",
+    width: '100%',
 
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     gap: 16,
   },
   content: {
     flex: 1,
-    width: "100%",
+    width: '100%',
     gap: 8,
   },
   page_heading: {
-    alignItems: "center",
-    width: "100%",
+    alignItems: 'center',
+    width: '100%',
   },
 });
 
