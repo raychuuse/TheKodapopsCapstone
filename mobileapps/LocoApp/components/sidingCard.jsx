523b206c249b7e74cb4727116c87f0446f2e6633
Andrew Wilks <a.wilksy92@gmail.com>
2024-04-22 19:02:15 +1000

    Refactor of the SidingCard and added the onPress prop to open the Siding Details Modal. LocoApp

--- mobileapps/LocoApp/components/sidingCard.jsx
+++ mobileapps/LocoApp/components/sidingCard.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
-import { View, StyleSheet, TouchableOpacity, Animated } from "react-native";
-import { MaterialIcons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { useState } from 'react';
+import { View, StyleSheet, TouchableOpacity, Animated } from 'react-native';
+import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 
 // Styles
-import { Title3, Headline, H1 } from "../styles/typography";
-import { Colours } from "../styles/colours";
+import { Title3, Headline, H1 } from '../styles/typography';
+import { Colours } from '../styles/colours';
 
-const SidingCard = ({ containerWidth, index, listLength, isCompleted = false, isSelected = false, name: name = "Siding Name", drop = 888, collect = 888, selectedHandeler, scrollX }) => {
+const SidingCard = ({
+  containerWidth,
+  index,
+  listLength,
+  isCompleted = false,
+  isSelected = false,
+  name: name = 'Siding Name',
+  drop = 888,
+  collect = 888,
+  selectedHandeler,
+  scrollX,
+  onPress,
+}) => {
   const [_isCompleted, setIsCompleted] = useState(isCompleted);
   const [_isSelected, setIsSelected] = useState(isSelected);
   const inputRange = [(index - 1) * 170, index * 170, (index + 1) * 170];
@@ -30,37 +42,140 @@ const SidingCard = ({ containerWidth, index, listLength, isCompleted = false, is
         _isCompleted ? styles.complete : _isSelected ? styles.selected : null,
         { transform: [{ translateY }] },
         index == 0 ? { marginLeft: (containerWidth - 170) / 2 } : null,
-        index == listLength - 1 ? { marginRight: (containerWidth - 170) / 2 } : null,
-      ]}>
-      <View style={{ flex: 3, justifyContent: "center" }}>
-        <Title3 style={[styles.defaultText, _isCompleted ? styles.completeText : _isSelected ? styles.selectedText : null]}>{name}</Title3>
-      </View>
-      <View style={styles.devider} />
-      <View style={styles.binCounterContainer}>
-        <View style={styles.binCounterLabel}>
-          <MaterialIcons name="download" size={18} color={(Colours.spPendingText, _isCompleted ? Colours.spCompleteText : _isSelected ? Colours.spSelectedText : null)} />
-          <Headline style={[{ marginRight: 9 }, styles.defaultText, _isCompleted ? styles.completeText : _isSelected ? styles.selectedText : null]}>Drop Off</Headline>
+        index == listLength - 1
+          ? { marginRight: (containerWidth - 170) / 2 }
+          : null,
+      ]}
+    >
+      <TouchableOpacity
+        onPress={onPress}
+        style={{ flex: 1, width: '100%', alignItems: 'center' }}
+      >
+        <View style={{ flex: 3, justifyContent: 'center' }}>
+          <Title3
+            style={[
+              styles.defaultText,
+              _isCompleted
+                ? styles.completeText
+                : _isSelected
+                ? styles.selectedText
+                : null,
+            ]}
+          >
+            {name}
+          </Title3>
         </View>
-        <H1 style={[styles.binCounter, _isCompleted ? styles.completeText : _isSelected ? styles.selectedText : null]}>{drop}</H1>
-      </View>
-      <View style={styles.devider} />
-      <View style={styles.binCounterContainer}>
-        <View style={styles.binCounterLabel}>
-          <MaterialIcons name="upload" size={18} color={(Colours.spPendingText, _isCompleted ? Colours.spCompleteText : _isSelected ? Colours.spSelectedText : null)} />
-          <Headline style={[{ marginRight: 9 }, styles.defaultText, _isCompleted ? styles.completeText : _isSelected ? styles.selectedText : null]}>Collect</Headline>
+        <View style={styles.devider} />
+        <View style={styles.binCounterContainer}>
+          <View style={styles.binCounterLabel}>
+            <MaterialIcons
+              name='download'
+              size={18}
+              color={
+                (Colours.spPendingText,
+                _isCompleted
+                  ? Colours.spCompleteText
+                  : _isSelected
+                  ? Colours.spSelectedText
+                  : null)
+              }
+            />
+            <Headline
+              style={[
+                { marginRight: 9 },
+                styles.defaultText,
+                _isCompleted
+                  ? styles.completeText
+                  : _isSelected
+                  ? styles.selectedText
+                  : null,
+              ]}
+            >
+              Drop Off
+            </Headline>
+          </View>
+          <H1
+            style={[
+              styles.binCounter,
+              _isCompleted
+                ? styles.completeText
+                : _isSelected
+                ? styles.selectedText
+                : null,
+            ]}
+          >
+            {drop}
+          </H1>
         </View>
-        <H1 style={[styles.binCounter, _isCompleted ? styles.completeText : _isSelected ? styles.selectedText : null]}>{collect}</H1>
-      </View>
-      <View style={styles.devider} />
-      <View style={{ flex: 3, justifyContent: "center" }}>
-        <TouchableOpacity onPress={selectedHandeler} disabled={isCompleted}>
-          <MaterialCommunityIcons
-            name={_isCompleted ? "checkbox-marked-circle-outline" : _isSelected ? "star-circle-outline" : "checkbox-blank-circle-outline"}
-            size={36}
-            color={(Colours.spPendingText, _isCompleted ? Colours.spCompleteText : _isSelected ? Colours.spSelectedText : null)}
-          />
-        </TouchableOpacity>
-      </View>
+        <View style={styles.devider} />
+        <View style={styles.binCounterContainer}>
+          <View style={styles.binCounterLabel}>
+            <MaterialIcons
+              name='upload'
+              size={18}
+              color={
+                (Colours.spPendingText,
+                _isCompleted
+                  ? Colours.spCompleteText
+                  : _isSelected
+                  ? Colours.spSelectedText
+                  : null)
+              }
+            />
+            <Headline
+              style={[
+                { marginRight: 9 },
+                styles.defaultText,
+                _isCompleted
+                  ? styles.completeText
+                  : _isSelected
+                  ? styles.selectedText
+                  : null,
+              ]}
+            >
+              Collect
+            </Headline>
+          </View>
+          <H1
+            style={[
+              styles.binCounter,
+              _isCompleted
+                ? styles.completeText
+                : _isSelected
+                ? styles.selectedText
+                : null,
+            ]}
+          >
+            {collect}
+          </H1>
+        </View>
+        <View style={styles.devider} />
+        <View style={{ flex: 3, justifyContent: 'center' }}>
+          <TouchableOpacity
+            onPress={selectedHandeler}
+            disabled={isCompleted}
+          >
+            <MaterialCommunityIcons
+              name={
+                _isCompleted
+                  ? 'checkbox-marked-circle-outline'
+                  : _isSelected
+                  ? 'star-circle-outline'
+                  : 'checkbox-blank-circle-outline'
+              }
+              size={36}
+              color={
+                (Colours.spPendingText,
+                _isCompleted
+                  ? Colours.spCompleteText
+                  : _isSelected
+                  ? Colours.spSelectedText
+                  : null)
+              }
+            />
+          </TouchableOpacity>
+        </View>
+      </TouchableOpacity>
     </Animated.View>
   );
 };
@@ -68,36 +183,36 @@ const SidingCard = ({ containerWidth, index, listLength, isCompleted = false, is
 const styles = StyleSheet.create({
   binCounterContainer: {
     flex: 6,
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
   },
   binCounterLabel: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     gap: 4,
   },
   binCounter: {
-    textAlign: "center",
+    textAlign: 'center',
     fontSize: 51,
-    fontWeight: "800",
+    fontWeight: '800',
     lineHeight: 61,
     color: Colours.spPendingText,
     marginVertical: 0,
   },
   devider: {
     height: 1,
-    width: "70%",
+    width: '70%',
     backgroundColor: Colours.spPendingText,
   },
   debug: {
-    borderStyle: "dashed",
-    borderColor: "red",
+    borderStyle: 'dashed',
+    borderColor: 'red',
     borderWidth: 1,
   },
   card: {
-    borderStyle: "solid",
-    borderColor: "#fff",
+    borderStyle: 'solid',
+    borderColor: '#fff',
     borderTopWidth: 4,
     borderLeftWidth: 2,
     borderRightWidth: 2,
@@ -105,8 +220,8 @@ const styles = StyleSheet.create({
     // margin: 10,
     padding: 10,
     width: 170,
-    alignItems: "center",
-    height: "90%",
+    alignItems: 'center',
+    height: '90%',
   },
   default: {
     backgroundColor: Colours.spPending,
