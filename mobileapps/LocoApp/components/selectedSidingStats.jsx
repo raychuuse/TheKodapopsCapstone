09d86e1f928ac8eab626bd4c614b307e2c4d7b86
Andrew Wilks <a.wilksy92@gmail.com>
2024-04-22 19:01:12 +1000

    SelectedSiddingStats to use the global RunData state and open the Siding Details Modal.

--- mobileapps/LocoApp/components/selectedSidingStats.jsx
+++ mobileapps/LocoApp/components/selectedSidingStats.jsx
@@ -1,45 +1,111 @@
-import { View } from "react-native";
-import { MaterialIcons } from "@expo/vector-icons";
+import { TouchableOpacity, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 // Components
-import Container from "./container";
-import { Footnote, Headline, Title3 } from "../styles/typography";
-import { Colours } from "../styles/colours";
+import Container from './container';
+import { Footnote, Headline, Title3 } from '../styles/typography';
+import { Colours } from '../styles/colours';
 
-export default function SelectedSiddingStats() {
+// Import Mock Data
+import { RunMockData } from '../data/RunMockData';
+
+export default function SelectedSiddingStats({
+  runData = RunMockData,
+  selectedSidingID,
+  openSidingDetailsModal,
+}) {
   return (
-    <Container style={{ flex: 2, marginLeft: 256, flexDirection: "row", alignItems: "center" }}>
+    <Container
+      style={{
+        flex: 2,
+        marginLeft: 256,
+        flexDirection: 'row',
+        alignItems: 'center',
+      }}
+    >
       {/* Drop Off */}
-      <View style={{ flex: 2, alignItems: "center" }}>
-        <View style={{ flexDirection: "row", alignItems: "center", gap: 4 }}>
-          <MaterialIcons name="download" size={18} color={Colours.textLevel3} />
-          <Headline style={{ marginRight: 9, color: Colours.textLevel3 }}>Drop Off</Headline>
+      <TouchableOpacity
+        style={{ flex: 2, alignItems: 'center' }}
+        onPress={openSidingDetailsModal}
+      >
+        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 4 }}>
+          <MaterialIcons
+            name='download'
+            size={18}
+            color={Colours.textLevel3}
+          />
+          <Headline style={{ marginRight: 9, color: Colours.textLevel3 }}>
+            Drop Off
+          </Headline>
         </View>
-        <Title3>058</Title3>
-      </View>
+        <Title3>
+          {
+            runData.sidings.find((item) => item.id === selectedSidingID)
+              .binsDrop.length
+          }
+        </Title3>
+      </TouchableOpacity>
 
-      <View style={{ marginHorizontal: 4, borderStyle: "solid", borderColor: Colours.textLevel3, borderWidth: 0.25, height: "80%" }} />
+      <View
+        style={{
+          marginHorizontal: 4,
+          borderStyle: 'solid',
+          borderColor: Colours.textLevel3,
+          borderWidth: 0.25,
+          height: '80%',
+        }}
+      />
 
       {/* Siding */}
-      <View style={{ flex: 3, height: "100%", alignItems: "center" }}>
-        <View style={{ flexDirection: "row", alignItems: "center", gap: 4 }}>
-          <Footnote style={{ marginLeft: 9, color: Colours.textLevel3 }}>Selected Siding</Footnote>
-          <MaterialIcons name="edit-location-alt" size={18} color={Colours.textLevel3} />
+      <View style={{ flex: 3, height: '100%', alignItems: 'center' }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 4 }}>
+          <Footnote style={{ marginLeft: 9, color: Colours.textLevel3 }}>
+            Selected Siding
+          </Footnote>
+          <MaterialIcons
+            name='edit-location-alt'
+            size={18}
+            color={Colours.textLevel3}
+          />
         </View>
-        <Title3>Siding Name</Title3>
+        <Title3>
+          {runData.sidings.find((item) => item.id === selectedSidingID).name}
+        </Title3>
         <Footnote style={{ color: Colours.textLevel3 }}>ETA: 12:05 PM</Footnote>
       </View>
 
-      <View style={{ marginHorizontal: 4, borderStyle: "solid", borderColor: Colours.textLevel3, borderWidth: 0.25, height: "80%" }} />
+      <View
+        style={{
+          marginHorizontal: 4,
+          borderStyle: 'solid',
+          borderColor: Colours.textLevel3,
+          borderWidth: 0.25,
+          height: '80%',
+        }}
+      />
 
       {/* Collect */}
-      <View style={{ flex: 2, alignItems: "center" }}>
-        <View style={{ flexDirection: "row", alignItems: "center", gap: 4 }}>
-          <Headline style={{ marginLeft: 9, color: Colours.textLevel3 }}>Collect</Headline>
-          <MaterialIcons name="upload" size={18} color={Colours.textLevel3} />
+      <TouchableOpacity
+        style={{ flex: 2, alignItems: 'center' }}
+        onPress={openSidingDetailsModal}
+      >
+        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 4 }}>
+          <Headline style={{ marginLeft: 9, color: Colours.textLevel3 }}>
+            Collect
+          </Headline>
+          <MaterialIcons
+            name='upload'
+            size={18}
+            color={Colours.textLevel3}
+          />
         </View>
-        <Title3>058</Title3>
-      </View>
+        <Title3>
+          {
+            runData.sidings.find((item) => item.id === selectedSidingID)
+              .binsCollect.length
+          }
+        </Title3>
+      </TouchableOpacity>
     </Container>
   );
 }
