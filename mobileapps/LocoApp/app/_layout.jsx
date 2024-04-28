80b1f9699ca795390c1097da6e4b3fa91786b509
Andrew Wilks <a.wilksy92@gmail.com>
2024-04-28 21:48:12 +1000

    added the modal context provider to the main layout.

--- mobileapps/LocoApp/app/_layout.jsx
+++ mobileapps/LocoApp/app/_layout.jsx
@@ -1,24 +1,29 @@
 import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+// Import Providers
 import { ThemeProvider } from '../styles/themeContext';
 import { RunProvider } from '../context/runContext';
+import { ModalProvider } from '../context/modalContext';
 
 export default function HomeLayout() {
   return (
     <SafeAreaProvider>
       <ThemeProvider>
         <RunProvider>
-          <SafeAreaView
-            style={{
-              flex: 1,
-              position: 'relative',
-              backgroundColor: '#272231',
-            }}
-          >
-            <Slot />
-            <StatusBar style='light' />
-          </SafeAreaView>
+          <ModalProvider>
+            <SafeAreaView
+              style={{
+                flex: 1,
+                position: 'relative',
+                backgroundColor: '#272231',
+              }}
+            >
+              <Slot />
+              <StatusBar style='light' />
+            </SafeAreaView>
+          </ModalProvider>
         </RunProvider>
       </ThemeProvider>
     </SafeAreaProvider>
