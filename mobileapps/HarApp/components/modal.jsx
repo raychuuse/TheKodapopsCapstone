69bc12bfee59dd7cc87121be8d563a448e37a7e9
Andrew Wilks <a.wilksy92@gmail.com>
2024-05-06 16:23:34 +1000

    Changed how the Modal is structured. HarApp

--- mobileapps/HarApp/components/modal.jsx
+++ mobileapps/HarApp/components/modal.jsx
@@ -1,36 +1,54 @@
-import React from "react";
-import { Modal, StyleSheet, View, TouchableOpacity } from "react-native";
-import { BlurView } from "expo-blur";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { selectionAsync } from "expo-haptics";
+import React from 'react';
+import { Modal, StyleSheet, View, TouchableOpacity } from 'react-native';
+import { BlurView } from 'expo-blur';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { selectionAsync } from 'expo-haptics';
 
-const CustomModal = ({ isVisible, onClose, children, buttonIcon = "close-circle-outline", style }) => {
+const CustomModal = ({
+  isVisible,
+  onClose,
+  children,
+  buttonIcon = 'close-circle-outline',
+  style,
+}) => {
   return (
-    <Modal transparent={true} visible={isVisible} animationType="fade" onRequestClose={onClose}>
-      <TouchableOpacity
-        style={styles.container}
-        activeOpacity={1}
-        onPressOut={() => {
-          onClose();
-          selectionAsync();
-        }} // When the user taps outside the modal content, it will close
-      >
-        <BlurView intensity={100} style={styles.blurView} tint="dark">
+    <Modal
+      transparent={true}
+      visible={isVisible}
+      animationType='fade'
+      onRequestClose={onClose}
+    >
+      <View style={styles.container}>
+        <BlurView
+          intensity={100}
+          style={styles.blurView}
+          tint='dark'
+        />
+        <TouchableOpacity
+          style={styles.modalBackdrop}
+          activeOpacity={1}
+          onPressOut={() => {
+            onClose();
+            selectionAsync();
+          }}
+        />
+        <View style={[styles.modalContent, style]}>
+          {children}
           <TouchableOpacity
-            activeOpacity={1} // Prevents the press from triggering the BlurView's onPressOut
-            style={[styles.modalContent, style]}>
-            {children}
-            <TouchableOpacity
-              style={styles.closeButton}
-              onPress={() => {
-                onClose();
-                selectionAsync();
-              }}>
-              <MaterialCommunityIcons name={buttonIcon} size={56} color="white" />
-            </TouchableOpacity>
+            style={styles.closeButton}
+            onPress={() => {
+              onClose();
+              selectionAsync();
+            }}
+          >
+            <MaterialCommunityIcons
+              name={buttonIcon}
+              size={56}
+              color='white'
+            />
           </TouchableOpacity>
-        </BlurView>
-      </TouchableOpacity>
+        </View>
+      </View>
     </Modal>
   );
 };
@@ -38,22 +56,30 @@ const CustomModal = ({ isVisible, onClose, children, buttonIcon = "close-circle-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalBackdrop: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   blurView: {
-    width: "100%",
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
   },
   modalContent: {
-    width: "80%",
-    backgroundColor: "white",
+    width: '80%',
+    backgroundColor: 'white',
     borderRadius: 32,
     padding: 20,
-    alignItems: "center",
-    shadowColor: "#000",
+    alignItems: 'center',
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
       height: 2,
@@ -61,10 +87,9 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
-    position: "relative",
   },
   closeButton: {
-    position: "absolute",
+    position: 'absolute',
     bottom: -76,
     left: 0,
   },
