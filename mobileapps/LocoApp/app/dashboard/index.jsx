801ff5f4461724a69f171dd50b456e6fa5cd70f5
Andrew Wilks <a.wilksy92@gmail.com>
2024-05-09 22:45:13 +1000

    Added the dot indicators for the bottom of the dashboard. LocoApp

--- mobileapps/LocoApp/app/dashboard/index.jsx
+++ mobileapps/LocoApp/app/dashboard/index.jsx
@@ -1,5 +1,5 @@
-import { View, Animated } from 'react-native';
-import React, { useState } from 'react';
+import { View, Animated, Dimensions } from 'react-native';
+import React, { useRef, useState } from 'react';
 
 // Components
 import StatusIndicator from '../../components/statusIndicator';
@@ -18,6 +18,7 @@ import { NotificationsMockData } from '../../data/NotificationsMockData';
 // Import Providers
 import { useTheme } from '../../styles/themeContext';
 import { useRun } from '../../context/runContext';
+import SidingListScrollBar from '../../components/sidingListScrollBar';
 
 export default function Page() {
   // Providers
@@ -26,6 +27,9 @@ export default function Page() {
 
   const scrollX = React.useRef(new Animated.Value(0)).current;
   const [sidingCarouselWidth, setSidingCarouselWidth] = useState(0);
+
+  const sidingListRef = useRef(null);
+  const windowWidth = Dimensions.get('window').width;
 
   // Run Data for the Whole App
   const [notifications, setNotifications] = useState(NotificationsMockData);
@@ -74,6 +78,7 @@ export default function Page() {
             }}
           >
             <Animated.FlatList
+              ref={sidingListRef}
               contentContainerStyle={{
                 alignItems: 'center',
                 paddingVertical: 10,
@@ -100,13 +105,11 @@ export default function Page() {
               )}
             />
           </View>
-          <View
-            style={{
-              minHeight: 56,
-              backgroundColor: theme.containerGradient[0],
-              borderRadius: 16,
-            }}
-          ></View>
+          <SidingListScrollBar
+            data={runData.sidings}
+            flatListRef={sidingListRef}
+            containerWidth={sidingCarouselWidth}
+          />
         </View>
       </View>
       {/* Nav Bar */}
