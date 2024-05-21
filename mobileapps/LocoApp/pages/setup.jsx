f9cdd682d3cfd0bc1d0163126e87a34e3803bbf0
Andrew Wilks <a.wilksy92@gmail.com>
2024-05-21 22:07:16 +1000

    fixed missing background colours in the setup page. LocoApp

--- mobileapps/LocoApp/pages/setup.jsx
+++ mobileapps/LocoApp/pages/setup.jsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Link, router} from 'expo-router';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Link, router } from 'expo-router';
 
 // Import Components
 import Button from '../components/button';
@@ -10,82 +10,84 @@ import SettingsItem from '../components/settingsItem';
 // Import Styling Components
 import { LargeTitle, Title1 } from '../styles/typography';
 import { useTheme } from '../styles/themeContext';
-import {Colours} from '../styles/colours';
 
 // Import Mock Data
-import {getAllLocos} from "../api/loco.api";
-import {useRun} from "../context/runContext";
-import {errorToast} from "../lib/alerts";
+import { getAllLocos } from '../api/loco.api';
+import { useRun } from '../context/runContext';
+import { errorToast } from '../lib/alerts';
 
 const SetupPage = () => {
-    const [locos, setLocos] = useState();
-    const {getLocoID, setLocoID, onRunStarted} = useRun();
+  const [locos, setLocos] = useState();
+  const { getLocoID, setLocoID, onRunStarted } = useRun();
+  const { theme } = useTheme();
 
-    useEffect(() => {
-        getAllLocos()
-            .then(response => {
-                setLocos(response);
-            })
-            .catch(err => {
-                console.error(err);
-                errorToast(err);
-            });
-    }, []);
+  useEffect(() => {
+    getAllLocos()
+      .then((response) => {
+        setLocos(response);
+      })
+      .catch((err) => {
+        console.error(err);
+        errorToast(err);
+      });
+  }, []);
 
-    const onLocoSelected = (locoID) => {
-        setLocoID(locoID);
-    }
+  const onLocoSelected = (locoID) => {
+    setLocoID(locoID);
+  };
 
-    const onStartPressed = () => {
-        const loco = locos?.find(l => l.locoID === getLocoID());
-        if (loco == null) return;
-        onRunStarted();
-    }
+  const onStartPressed = () => {
+    const loco = locos?.find((l) => l.locoID === getLocoID());
+    if (loco == null) return;
+    onRunStarted();
+  };
 
-    return (
-        <View style={styles.page}>
-            <View style={styles.body}>
-                {/* Page Headings */}
-                <View style={styles.page_heading}>
-                    <LargeTitle>
-                        <GreetingMessage/>
-                    </LargeTitle>
-                    <Title1>John Smith</Title1>
-                </View>
-                {/* Page Content */}
-                <View style={styles.content}>
-                    {/* Locomotive Selector */}
-                    {locos != undefined && <SettingsItem
-                        label='Locomotive'
-                        options={locos?.map(loco => {
-                            return {id: loco.locoID, label: loco.locoName};
-                        })}
-                        setSelectedItem={onLocoSelected}
-                    />}
-                </View>
-                {/* Actions */}
-                <View style={styles.actions}>
-                    <Link
-                        href='/'
-                        asChild
-                    >
-                        <Button
-                            title='Log Out'
-                            textColor={Colours.textLevel2}
-                            backgroundColor='transparent'
-                            border
-                        />
-                    </Link>
-                    <Button
-                        title='Start'
-                        textColor={Colours.textLevel3}
-                        style={StyleSheet.create({flex: 1})}
-                        onPress={onStartPressed}
-                    />
-                </View>
-            </View>
+  return (
+    <View style={[styles.page, { backgroundColor: theme.appBG }]}>
+      <View style={[styles.body, { backgroundColor: theme.bgModal }]}>
+        {/* Page Headings */}
+        <View style={styles.page_heading}>
+          <LargeTitle>
+            <GreetingMessage />
+          </LargeTitle>
+          <Title1>John Smith</Title1>
         </View>
-    );
+        {/* Page Content */}
+        <View style={styles.content}>
+          {/* Locomotive Selector */}
+          {locos != undefined && (
+            <SettingsItem
+              label='Locomotive'
+              options={locos?.map((loco) => {
+                return { id: loco.locoID, label: loco.locoName };
+              })}
+              setSelectedItem={onLocoSelected}
+            />
+          )}
+        </View>
+        {/* Actions */}
+        <View style={styles.actions}>
+          <Link
+            href='/'
+            asChild
+          >
+            <Button
+              title='Log Out'
+              textColor={theme.textLevel2}
+              backgroundColor='transparent'
+              border
+            />
+          </Link>
+          <Button
+            title='Start'
+            textColor={theme.textLevel3}
+            style={StyleSheet.create({ flex: 1 })}
+            onPress={onStartPressed}
+          />
+        </View>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
