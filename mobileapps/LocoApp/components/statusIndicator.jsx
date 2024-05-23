ae506931dd867601680a6c862e03a4f7d0a39448
Zac <zac.gillerat@hotmail.com>
2024-05-23 20:37:15 +1000

    Loco: Show loco name

--- mobileapps/LocoApp/components/statusIndicator.jsx
+++ mobileapps/LocoApp/components/statusIndicator.jsx
@@ -7,15 +7,20 @@ import NetworkIndicator from "./networkIndicator";
 
 // Styles
 import { Title2, Title1 } from "../styles/typography";
+import { useRun } from '../context/runContext';
 
 export default function StatusIndicator() {
+  const {getLoco} = useRun();
+
+  const loco = getLoco();
+
   return (
     <Container style={{ flexDirection: "row" }}>
       <NetworkIndicator style={{ paddingRight: 12 }} />
 
       {/* Config Status */}
       <View style={{ paddingHorizontal: 24 }}>
-        <Title1>Loco #12</Title1>
+        <Title1>{loco != null ? loco.locoName : ''}</Title1>
         <View>
           <Title2 style={{ textAlign: "center" }}>
             <Clock />
