5ddfe9625baef4f7fc295bd208a4c77fc41a0c8e
raychuuse <raychuuse@gmail.com>
2024-05-24 04:39:38 +1000

    Removed old metric code and similar comments

--- OGApplication/mdrs/src/pages/Home.jsx
+++ OGApplication/mdrs/src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { DashboardMetrics } from "../components/DashboardMetrics";
 import { DashboardLogTables } from "../components/DashboardLogTable";
 import { DashboardTables } from "../components/DashboardTable";
 
@@ -11,30 +10,6 @@ import {useAuth} from "../AuthProvider";
 
 export default function Home() {
 
-    /*useEffect(() => {
-        const newSocket = new WebSocket('ws://localhost:8080/test')
-
-        newSocket.onopen = () => {
-            console.info('Connection');
-        };
-
-        newSocket.onmessage = message => {
-            console.info('Received', message);
-        };
-
-        newSocket.onclose = () => {
-            console.info('Closed');
-        };
-
-        setInterval(() => {
-            newSocket.send('Testing a');
-        }, 5000);
-
-        return () => {
-            newSocket.close();
-        };
-    }, []);*/
-
     return (
         <main>
             <section className="hero">
@@ -42,10 +17,6 @@ export default function Home() {
                     <h1 className="hero__title">Dashboard</h1>
                     <p className="hero__subtitle"></p>
                     <br></br>
-                    {/* Overview Metrics */}
-                    <div className="row">
-                        {/**<DashboardMetrics />**/}
-                    </div>
                     {/* Overview Tables */}
                     <div className="row">
                         <hr></hr>
